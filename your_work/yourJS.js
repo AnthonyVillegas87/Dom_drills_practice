6fc9a73ee3497147a05cf3519bb192deecfd7d55
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-29 20:26:08 -0500

    feat: pt 2 for random num generator in ea. btn

--- your_work/yourJS.js
+++ your_work/yourJS.js
@@ -1,6 +1,16 @@
 $(document).ready(() => {
 
 
+$('#num-generator').on('click', () => {
+    let number = Math.floor(Math.random() * 16) + 1;
+    $('.card-text').html(number);
+    $('span').each(function()  {
+        let spanNumber = Math.floor(Math.random() * 10) + 1;
+        $(this).html(spanNumber);
+    })
+
+});
+
 
 
 
